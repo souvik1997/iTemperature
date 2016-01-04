706816560d2303db9f995e151daadb3d28335e23
Souvik Banerjee <souvik1997@gmail.com>
2016-01-04 14:55:16 -0600

    Initial Commit

---
+++ iTemperature/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iTemperature
+//
+//  Created by Souvik Banerjee on 1/4/16.
+//  Copyright © 2016 Souvik Banerjee. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iTemperature/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iTemperature
+//
+//  Created by Souvik Banerjee on 1/4/16.
+//  Copyright © 2016 Souvik Banerjee. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
